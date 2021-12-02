bcbaf629663bb4b7c67ab9c03dd9fcf320136567
sim1pas <simon.pascal.ngos-ext@fnacdarty.com>
2021-12-02 12:28:00 +0100

    retirer ceci

--- src/main/apps/src/index.js
+++ src/main/apps/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM  from "react-dom"; 
-//import './index.scss';
-import NavigationComponent from './js/components/NavigationComponent';
 import RouteComponent from './js/components/RouteComponent';
 
 const wrapper = document.getElementById("container");
 
 wrapper? ReactDOM.render(
-    /*<React.StrictMode>
-        <NavigationComponent />
-    </React.StrictMode>*/
     <React.StrictMode>
         <RouteComponent />
     </React.StrictMode>
